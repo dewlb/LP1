1477609a7aa47af8822fadaa0463952943ad4714
Ethan Jewell <157914835+dewlb@users.noreply.github.com>
2024-11-06 16:34:47 -0500

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from './pages/login';
+import LoginPage from './pages/LoginPage.tsx';
 import './App.css'
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
     <>
       <Router>
         <Routes>
+          <LoginPage />
           <Route path='/login' element={<Login />}/>
         </Routes>
       </Router>
