6e41fddbedec42c84d1b87a73567e648599df75f
sami <saminajib2000@hotmail.com>
2024-11-27 14:56:07 -0500

    added unit testing only works local since jest dependency is only meant for dev

---
+++ server/tests/createTournament.test.js
@@ -0,0 +1,39 @@
+import request from 'supertest';
+import router from '../server.js'; 
+
+describe('POST /createTournament', () => {
+
+    test('should create a new tournament', async () => {
+        const response = await request(router)
+            .post('/api/createTournament')
+            .send({
+                name: 'Test Tournament',
+                userID: '12345',
+                size: 16,
+            });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body.message).toBe('Tournament created succesfully');
+        expect(response.body.info).toMatchObject({
+            name: 'Test Tournament',
+            owner: '12345',
+            max_size: 16,
+            current_size: 0,
+            participants: [],
+        });
+    });
+
+    test('should not allow duplicate tournament names', async () => {
+
+        const response = await request(router)
+            .post('/api/createTournament')
+            .send({
+                name: 'Test Tournament',
+                userID: '67890',
+                size: 16,
+            });
+
+        expect(response.statusCode).toBe(409);
+        expect(response.body.error).toBe('Tournament with this name already exists');
+    });
+});
