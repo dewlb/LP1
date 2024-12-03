40f7d489790d01b6bfc1abf6d76e0cceb6b0b07a
MagicMelody2 <127923573+MagicMelody2@users.noreply.github.com>
2024-12-03 10:19:30 -0500

    Update App.tsx
    Handles create tournament progress bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,21 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import HomePage from './pages/HomePage';
-import Dashboard from './pages/Dashboard';
-import MyProfile from './pages/MyProfile';
-import Settings from './pages/Settings';
-import Connect from './pages/Connect';
-import AboutUs from './pages/AboutUs';
-import AboutPage from './pages/AboutPage';
-import CreateTournament from './pages/CreateTournament';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+import Dashboard from "./pages/Dashboard";
+import MyProfile from "./pages/MyProfile";
+import Settings from "./pages/Settings";
+import Connect from "./pages/Connect";
+import AboutUs from "./pages/AboutUs";
+import AboutPage from "./pages/AboutPage";
+import CreateTournament from "./pages/CreateTournament";
+import ProgressBar from "./pages/ProgressBar";
 
 function App() {
   return (
@@ -23,25 +28,36 @@ function App() {
 // MainContent handles the logic for conditionally rendering Navbar
 function MainContent() {
   const location = useLocation();
-  const hideNavbarRoutes = ['/login', '/register', '/dashboard', '/aboutus', '/MyProfile', '/Settings', '/connect', '/createTournament']; // Add other routes where Navbar should be hidden
+  const hideNavbarRoutes = [
+    "/login",
+    "/register",
+    "/dashboard",
+    "/aboutus",
+    "/MyProfile",
+    "/Settings",
+    "/connect",
+    "/createTournament",
+    "/progressBar",
+  ]; // Add other routes where Navbar should be hidden
 
   return (
     <>
       {/* Conditionally render Navbar based on the current route */}
       {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
-      
+
       {/* Define your Routes */}
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/about" element = {<AboutPage />} />
+        <Route path="/about" element={<AboutPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element = {<Dashboard />} />
-        <Route path="/createTournament" element = {<CreateTournament />} />
-        <Route path="/MyProfile" element = {<MyProfile />} />
-        <Route path="/Settings" element = {<Settings />} />
-        <Route path="/connect" element = {<Connect />} />
-        <Route path="/aboutus" element = {<AboutUs />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/createTournament" element={<CreateTournament />} />
+        <Route path="/MyProfile" element={<MyProfile />} />
+        <Route path="/Settings" element={<Settings />} />
+        <Route path="/connect" element={<Connect />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/progressBar" element={<ProgressBar currentStep={1} />} />
       </Routes>
     </>
   );
