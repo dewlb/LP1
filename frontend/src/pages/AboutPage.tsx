9e252904a929db8d6b9b28be7b1e2ee88702179c
MagicMelody2 <127923573+MagicMelody2@users.noreply.github.com>
2024-12-03 10:24:58 -0500

    Update AboutPage.tsx
    Added list of sports and updated description

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,37 +1,73 @@
-
 import "./AboutPage.css";
 
 function AboutPage() {
-    return(
-            <>
-            <div className="desc-container">
-                <h2>Create your GamePlan.</h2>
-                <p>GamePlan offers users the ability to create and manage their tournaments.
-                Users input their desired tournament name, its initial size, its current size, and the tournament's participants.
-                As the sporting event progresses, users can update the tournament as needed.
-                This makes creating and managing tournaments easy.</p>
-            </div>
+  return (
+    <>
+      <div className="desc-container">
+        <h2 className="desc-label">Create your GamePlan.</h2>
+        <p>
+          GamePlan is your all-in-one tool for organizing and managing local
+          sports tournaments. It allows you to focus on enjoying the game while
+          it takes care of the details. Set up and manage your tournaments
+          effortlessly—input the tournament name, participants, and brackets,
+          and let GamePlan handle the rest. As the event progresses, you can
+          update everything in real-time. Work smarter, not harder, and let
+          GamePlan do the work for you!
+        </p>
 
-            <div className="team-container">
-                <h1>Meet our team.</h1>
-                <h3>Ethan Jewell: Project Manager</h3>
-                <h3>Garret Miller: Database</h3>
-                <h3>Hector Ramos: API</h3>
-                <h3>Sami Najib: API</h3>
-                <h3>Gracie Doherty: Front End Developer</h3>
-                <h3>Veronica Vargas: Front End Developer</h3>
-                <h3>Alex Beaufort: Mobile Developer</h3>
-            </div>
-            </>
-    );
+        <div className="sports-container">
+          <div className="sports-item">Badminton</div>
+          <div className="sports-item">Baseball</div>
+          <div className="sports-item">Basketball</div>
+          <div className="sports-item">Bowling</div>
+          <div className="sports-item">Cheerleading</div>
+          <div className="sports-item">Cornhole</div>
+          <div className="sports-item">Cycling</div>
+          <div className="sports-item">Disc Golf</div>
+          <div className="sports-item">Dodgeball</div>
+          <div className="sports-item">Fencing</div>
+          <div className="sports-item">Flag Football</div>
+          <div className="sports-item">Golf</div>
+          <div className="sports-item">Ice Hockey</div>
+          <div className="sports-item">Jiu Jitsu</div>
+          <div className="sports-item">Judo</div>
+          <div className="sports-item">Karate</div>
+          <div className="sports-item">Kendo</div>
+          <div className="sports-item">Kiteboarding</div>
+          <div className="sports-item">Lacrosse</div>
+          <div className="sports-item">Paintball</div>
+          <div className="sports-item">Pickleball</div>
+          <div className="sports-item">Rock Climbing</div>
+          <div className="sports-item">Rowing</div>
+          <div className="sports-item">Rugby</div>
+          <div className="sports-item">Running</div>
+          <div className="sports-item">Sailing</div>
+          <div className="sports-item">Soccer</div>
+          <div className="sports-item">Softball</div>
+          <div className="sports-item">Spikeball</div>
+          <div className="sports-item">Surfing</div>
+          <div className="sports-item">Swimming</div>
+          <div className="sports-item">Table Tennis</div>
+          <div className="sports-item">Tae Kwon Do</div>
+          <div className="sports-item">Tennis</div>
+          <div className="sports-item">Ultimate Frisbee</div>
+          <div className="sports-item">Volleyball</div>
+          <div className="sports-item">Wrestling</div>
+        </div>
+      </div>
+
+      <div className="team-container">
+        <h1>Meet our team!</h1>
+        <h3>Ethan Jewell: Project Manager</h3>
+        <h3>Garret Miller: Database</h3>
+        <h3>Hector Ramos: API</h3>
+        <h3>Sami Najib: API</h3>
+        <h3>Gracie Doherty: Front End Developer</h3>
+        <h3>Veronica Vargas: Front End Developer</h3>
+        <h3>Alex Beaufort: Mobile Developer</h3>
+      </div>
+    </>
+  );
 }
-
-
-
-
-
-
-
-
 
 export default AboutPage;
