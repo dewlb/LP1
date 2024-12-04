16c190648eef4150d8b02cdd1489c26e9bc1ce3b
sami <saminajib2000@hotmail.com>
2024-12-03 20:50:10 -0500

    added reset password

--- server/server.js
+++ server/server.js
@@ -5,13 +5,16 @@ import createTournament from './routes/create-tournament.js'
 import searchTournaments from './routes/search-tournaments.js'
 import updateTournament from './routes/update-tournament.js'
 import deleteTournament from './routes/delete-tournament.js'
+import reset from './routes/reset-password.js'
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 //parse json for object format for request
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/api", register);
 app.use("/api", login);
@@ -19,6 +22,7 @@ app.use("/api", searchTournaments);
 app.use("/api", createTournament);
 app.use("/api", updateTournament);
 app.use("/api", deleteTournament);
+app.use("/api", reset);
 
 app.get("/", (req, res) => {
     res.send("server is running");
